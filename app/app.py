62e7b9a8bb68dfe7285f2fbe2b57136d008a3672
Jhon Sanchez <iroh314@gmail.com>
2022-04-17 21:21:04 -0500

    Se realiza ajustes en archivo py

--- app/app.py
+++ app/app.py
@@ -16,6 +16,8 @@ def habitaciones():
 
 @app.route('/contactenos')
 def contactenos():
+
+    diccionario = [{'Ciudad':'Medellín', 'Dirección':'Calle 67 # 56-10', 'Nro Habitaciones':3, 'Precio':150000},{'Ciudad':'Cartagena', 'Dirección':'Carrera 34 # 23-10', 'Nro Habitaciones':2, 'Precio':180000},{'Ciudad':'Santa Marta', 'Dirección':'Calle 78 # 45-47', 'Nro Habitaciones':4, 'Precio':160000}]
     return render_template('contactenos.html')
 
 
